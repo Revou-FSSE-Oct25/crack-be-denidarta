f6c60902c2bc68f43743e9ed938203bdf8ff874e
Deni Dwi Arta <denidarta@gmail.com>
2026-03-28 11:44:01 +0700

    feat: register all resource modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,18 @@ import { AssignmentsModule } from './modules/assignments/assignments.module';
 import { SubmissionsModule } from './modules/submissions/submissions.module';
 
 @Module({
-  imports: [ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]), DatabaseModule, UsersModule, CoursesModule, EnrollmentsModule, LearningMaterialsModule, ClassSessionsModule, AttendancesModule, AssignmentsModule, SubmissionsModule],
+  imports: [
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+    DatabaseModule,
+    UsersModule,
+    CoursesModule,
+    EnrollmentsModule,
+    LearningMaterialsModule,
+    ClassSessionsModule,
+    AttendancesModule,
+    AssignmentsModule,
+    SubmissionsModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
