7f4c913cef253aaddffc73f70e357100700bb6bc
Deni Dwi Arta <denidarta@gmail.com>
2026-03-28 11:43:07 +0700

    feat: scaffold submissions module

---
+++ src/modules/submissions/dto/create-submission.dto.ts
@@ -0,0 +1,39 @@
+import { IsBoolean, IsDateString, IsEnum, IsInt, IsNumber, IsOptional, IsString, MaxLength } from 'class-validator';
+import { SubmissionStatus } from '@prisma/client';
+
+export class CreateSubmissionDto {
+  @IsInt()
+  assignmentId: number;
+
+  @IsInt()
+  userId: number;
+
+  @IsOptional()
+  @IsString()
+  submissionText?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  fileUrl?: string;
+
+  @IsOptional()
+  @IsDateString()
+  submittedAt?: string;
+
+  @IsOptional()
+  @IsNumber()
+  grade?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  passed?: boolean;
+
+  @IsOptional()
+  @IsString()
+  feedback?: string;
+
+  @IsOptional()
+  @IsEnum(SubmissionStatus)
+  status?: SubmissionStatus;
+}
